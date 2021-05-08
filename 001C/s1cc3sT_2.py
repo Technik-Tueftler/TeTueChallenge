cc2a4e884e1455361743a0bbcb498437da0c6aa6
s1cci <tholukai@gmail.com>
2021-05-08 22:54:50 +0200

    ready for pullrequest

--- 001C/s1cc3sT_2.py
+++ 001C/s1cc3sT_2.py
@@ -1,3 +1,4 @@
+import operator
 from uuid import uuid4
 
 class player:
@@ -12,10 +13,12 @@ def main():
     
     list_player = [player("Max", 3, 50, 10), player("Moritz", 2, 20, 30), player("Witwe Bolte", 3, 49, 9), player("Mecke", 1, 10, 79), player("Lämpel", 3, 49, 10), player("Fritz", 2, 20, 31), player("Böck", 1, 10, 80)]
     
-    # Vielen Dank an coder2k wegen der Erklaerung der lambada-Funktion ;)
-    # Sortierschlüssel in der Reihenfolge der Priorisierung. playtime mit Minus weil weniger besser ist. 
-    # Reverse weil mehr secrets und kills besser sind.
-    list_player_sorted = sorted(list_player, key=lambda x: (x.secrets, -x.playtime_s, x.kills), reverse=True)
+    # operator.attrgetter soll wohl schneller sein als lambda-Funktion. Hier wird die niedrigste Prioritaet zuerst sortiert.
+    # Da die sorted-Methode bei gleichwertigen Attributen die Reihenfolge beibehaelt, kann mit mehrmaligem Aufruf von "unten nach oben"
+    # sortiert werden (Prioritaeten). Dies fuehrt am Ende zur richtigen Reihenfolge. Glaube ich xD
+    list_player_sorted = sorted(list_player, key=operator.attrgetter("kills"), reverse=True)
+    list_player_sorted = sorted(list_player_sorted, key=operator.attrgetter("playtime_s"))
+    list_player_sorted = sorted(list_player_sorted, key=operator.attrgetter("secrets"), reverse=True)
     
     for rank, element in enumerate(list_player_sorted, start=1):
         print(rank, element.name)
