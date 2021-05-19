0a1cb0360d2391a9022977b4b9216eb4721d8031
SvenKo <sven.a.korbmacher@gmail.com>
2021-05-19 21:24:16 +0200

    Revert "Add tests for each contributor. Measure the profiling performance to count the executed instructions instead of raw time to compare the algos with a stable "call" counting"
    This reverts commit 2bfa855a

--- 001C/s1cc3sT.py
+++ 001C/s1cc3sT.py
@@ -1,14 +1,24 @@
 from uuid import uuid4
 
-
 class player:
     def __init__(self, name, secrets=0, playtime_s=0, kills=0):
-        self.gameid = uuid4()  # create unique id
+        self.gameid = uuid4() # create unique id
         self.name = name
         self.secrets = secrets
-        self.playtime_s = playtime_s  # playing time in seconds
+        self.playtime_s = playtime_s # playing time in seconds
         self.kills = kills
 
+def main():
+    
+    list_player = [player("Max", 3, 50, 10), player("Moritz", 2, 20, 30), player("Witwe Bolte", 3, 49, 9), player("Mecke", 1, 10, 79), player("Lämpel", 3, 49, 10), player("Fritz", 2, 20, 31), player("Böck", 1, 10, 80)]
+    
+    # Vielen Dank an coder2k wegen der Erklaerung der lambada-Funktion ;)
+    # Sortierschlüssel in der Reihenfolge der Priorisierung. playtime mit Minus weil weniger besser ist. 
+    # Reverse weil mehr secrets und kills besser sind.
+    list_player_sorted = sorted(list_player, key=lambda x: (x.secrets, -x.playtime_s, x.kills), reverse=True)
+    
+    for rank, element in enumerate(list_player_sorted, start=1):
+        print(rank, element.name)
 
-def sort_player(player_list):
-    player_list.sort(key=lambda x: (x.secrets, -x.playtime_s, x.kills), reverse=True)
+if __name__ == "__main__":
+    main()
