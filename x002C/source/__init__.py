1ada32a89b9fcfa1e0283fc100cf6e1ed3690f74
helbaco <github.com@sombrasoft.de>
2021-06-09 16:19:30 +0200

    added imports for helbaco functions

--- x002C/source/__init__.py
+++ x002C/source/__init__.py
@@ -8,3 +8,11 @@ from x002C.source.technik_tueftler import count_words_from_line as count_words_f
 from x002C.source.technik_tueftler import count_sentences as count_sentences_tetue
 from x002C.source.technik_tueftler import count_quotes as count_quotes_tetue
 from x002C.source.technik_tueftler import count_character_all as count_character_all_tetue
+
+from x002C.source.helbaco import count_lines as count_lines_helba
+from x002C.source.helbaco import count_words as count_words_helba
+from x002C.source.helbaco import count_character as count_character_helba
+from x002C.source.helbaco import count_words_from_line as count_words_from_line_helba
+from x002C.source.helbaco import count_sentences as count_sentences_helba
+from x002C.source.helbaco import count_quotes as count_quotes_helba
+from x002C.source.helbaco import count_character_all as count_character_all_helba
