0b9fcd5b68c90219f816a885d9a9c993f5e92732
Kersten Schaller <kersten.schaller@outlook.com>
2021-06-19 15:57:41 +0200

    new approach using curses

--- x003C/source/fingerprint_invalid/fingerprint_invalid.py
+++ x003C/source/fingerprint_invalid/fingerprint_invalid.py
@@ -1,50 +1,80 @@
 import random
 import time
 
-from rich.live import Live
-#from rich.table import Table
-from rich.console import Console
+import curses
 
 import os
-   
+from threading import Thread   
 ###################################################
-def draw():
-    screen.draw()
+# async Keypress detection 
+
+key_pressed = False
+
+def detect_key_press():
+    global key_pressed
+    stdscr = curses.initscr()
+    key = stdscr.getch()
+    key_pressed = True
+    endCurses()
+
+
 ###################################################
 
+def endCurses():
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+#screen.getch()
+
+#curses.endwin()
+
 class Screen:
-    
-    
 
     def __init__(self):
         self.toggle = 1
-      
+        self.size = os.get_terminal_size()
+        self.cursesScreen = curses.initscr()
+        #self.cursesScreen = curses.newwin(self.size.lines,self.size.columns)
+        self.cursesScreen.keypad(0)
+        curses.noecho()
+        
     def toggleScreen(self):
-        size = os.get_terminal_size()
         self.lines = [] 
-        if self.toggle is 1:
+        if self.toggle == 1:
             c = '-'
             self.toggle = 0
         else:
             c = '+'
             self.toggle = 1
-        for _ in range(size.lines):
-            line = [c] * (size.columns - 1)
+        for _ in range(self.size.lines):
+            line = [c] * (self.size.columns - 1)
             self.lines.append(line)
 
     def draw(self):
-        console.clear_live()
         self.toggleScreen()
+        self.cursesScreen.clear()
         for line in self.lines:
-            line = '[blue]' + ''.join(line)
-            console.print(line)
+            line = ''.join(line)
+            l = len(line)
+            self.cursesScreen.addstr((line))
+        self.cursesScreen.refresh()
 
 ###################################################
-console = Console(force_terminal=True)
-screen = Screen()
-#screen.draw()
 
-with Live(draw(), refresh_per_second=4) as live:
-    while True:
-        #time.sleep(0.4)
-        live.update(draw())
+screen = Screen()
+
+def gameLoop():
+    screen.draw()
+    
+    
+    
+if __name__ == "__main__":
+    thread = Thread(target = detect_key_press)
+    thread.start() # keypress detection
+
+    while not key_pressed:
+        gameLoop()
+        time.sleep(1)
+    curses.endwin()
