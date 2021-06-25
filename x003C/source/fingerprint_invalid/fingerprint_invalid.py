079186e56d24eac89806e8803fa89387e9ed6556
Kersten Schaller <kersten.schaller@outlook.com>
2021-06-25 18:48:10 +0200

    large change

--- x003C/source/fingerprint_invalid/fingerprint_invalid.py
+++ x003C/source/fingerprint_invalid/fingerprint_invalid.py
@@ -1,41 +1,106 @@
 import locale
 import time
-
+import sys
 import curses
-
+import random
+from enum import Enum
 import os
-from threading import Thread   
+from threading import Thread  
+from curses import wrapper as cursesWrapper
 ###################################################
-# async Keypress detection 
-
-key_pressed = False
-curses.initscr()  
-curses.start_color()
-curses.curs_set(False)
-curses.noecho()
 
 
+###################################################
+chickenString = (
+r"             xx     \n"
+r"             / .|_  \n"
+r"            /(_)_<  \n"
+r"           /  (     \n"
+r"  ((____.-'    )    \n"
+r"   \\          /    \n"
+r"    \\'-.-.-'`/     \n"
+r"     \\______/      \n"
+r"       _|_\\_         "
+)
+
+tetueString = (
+r'\__   __/(  ____ \ __   __/|\     /|(  ____ \    (  ____ \|\     /|(  ___  )( \      ( \      (  ____ \( (    /|(  ____ \(  ____ \ \n'
+r'   ) (   | (    \/   ) (   | )   ( || (    \/    | (    \/| )   ( || (   ) || (      | (      | (    \/|  \  ( || (    \/| (    \/ \n'
+r'   | |   | (__       | |   | |   | || (__        | |      | (___) || (___) || |      | |      | (__    |   \ | || |      | (__     \n'
+r'   | |   |  __)      | |   | |   | ||  __)       | |      |  ___  ||  ___  || |      | |      |  __)   | (\ \) || | ____ |  __)    \n'
+r'   | |   | (         | |   | |   | || (          | |      | (   ) || (   ) || |      | |      | (      | | \   || | \_  )| (       \n'
+r'   | |   | (____/\   | |   | (___) || (____/\    | (____/\| )   ( || )   ( || (____/\| (____/\| (____/\| )  \  || (___) || (____/\ \n'
+r'   )_(   (_______/   )_(   (_______)(_______/    (_______/|/     \||/     \|(_______/(_______/(_______/|/    )_)(_______)(_______/ '
+)
+
+islandString = (
+r"           __        \n"                  
+r"          /. _\      \n"                    
+r"     .--.|/_/__      \n"                            
+r"    ''.--o/___  \    \n"
+r"     /.'o|_o  '.|    \n"
+r"    |/   |_|  ~'     \n"
+r"    '    |_|         \n"
+r"         |_|         \n"
+r"      ___|_|______   \n"
+r"  .'':. .|_| :. ..\  \n"
+r"/:.  .:::|_|.\ .   \ "
+)
 
 
-def detect_key_press():
-    global key_pressed
-    stdscr = curses.initscr()
-    key = stdscr.getch()
-    key_pressed = True
-    endCurses()
+cloudString1 = (  
+r"    ____ _  \n"
+r"  _(    `.) \n"
+r" ( (    ) ))\n"
+r"( (   )  _) \n"
+r" '.__)--'   "
+)
+cloudString2 = (
+r"  _ __   \n"
+r" ( (  )`.\n"
+r"( (    ))\n"
+r" `-(__.' "
+)
+ 
 
-def parseTxtToDrawableObject(parseString, Color):
-    x = 0
-    y = -1
+cloudString3 = (
+r" .-.. \n"
+r"(_)_))  "
+)
+
+BackGroundLayer = 0
+EastereggLayer = 1
+IslandLayer = 2
+CloudLayer = 10
+
+COLOR_WATER = 1
+COLOR_CLOUD = 2
+COLOR_SHADOW = 3
+COLOR_BROWN = 4
+
+###################################################
+
+
+def parseTxtToDrawableObject(parseString, Color, opaque = False):
+    x = -1
+    y = 0
     points = []
-    for c in parseString:
-        if c != " " and c != "\n":
-            points.append(GraphicsPoint(x, y, c , Color ))
-            x = x + 1
-        else: 
-            if c == "\n":
-                x = 0
+    for i in range( len(parseString)):
+        c = parseString[i]
+        x = x + 1
+        if (c != " ") | (opaque == True):
+            if (c == "n") & (parseString[i-1] == '\\'):
+                x = -1
                 y = y + 1
+            else:
+                if c == "\\":
+                    if i < (len(parseString)):
+                        if (c == "\\") & (parseString[i+1] == 'n'):
+                            continue
+                        else:
+                            points.append(GraphicsPoint(x, y, c , Color ))
+                else:
+                        points.append(GraphicsPoint(x, y, c , Color ))
     return points
 
 ###################################################
@@ -51,65 +116,125 @@ def endCurses():
 ###################################################
 
 class GraphicsPoint:
-      def __init__(self, x, y, c, color):
-            self.X = x
-            self.Y = y
-            self.C = c
-            self.Color = color
+    def __init__(self, x, y, c, color):
+        self.X = x
+        self.Y = y
+        self.C = c
+        self.Color = color
+
+    def __eq__(self, other):
+        if isinstance(other, GraphicsPoint):
+            return (self.X == other.X) and ( self.Y == other.Y)
+        return False
+   
 
 ###################################################
 class DrawableObject:
+    def __init__(self, Layer, objString, X, Y, Color, opaque = False):
+        self.x_offset = X
+        self.y_offset = Y
+        self.layer = Layer
+        self.points = parseTxtToDrawableObject(objString, Color,opaque)
+        # find max
+        self.width  = 0
+        self.height  = 0
+        for p in self.points:
+            if p.X > self.width:
+                self.width = p.X
+            if p.Y > self.height:
+                self.height = p.Y
+        Screen.addDrawable(self)
+
     def get(self):
         returnPoints = []
         for p in self.points:
             returnPoints.append(GraphicsPoint((p.X + self.x_offset),(p.Y + self.y_offset), p.C, p.Color))
-        return returnPoints   
+        return returnPoints  
+
+    def placeAt(self, X , Y):
+        self.x_offset = X
+        self.y_offset = Y
+
 
 ###################################################
 class MoveAbleObject(DrawableObject):
-    def __init__(self, Direction, Y): 
+    def __init__(self, Layer, objString, X , Y, Color, Direction, Speed, opaque = True):
+        super().__init__(Layer, objString, X , Y, Color, opaque)
+        self.speed = Speed
         self.direction = Direction
-        if Direction == "left":
-            self.x_offset = curses.COLS + 8
-        if Direction == "right":
-            self.x_offset = -5
-        self.y_offset = Y
-        self.cnt = 0
+       
 
     def move(self):
         if self.direction == "left":
-            self.x_offset = self.x_offset - 1
+            self.x_offset = self.x_offset - self.speed
         else:
             if self.direction == "right":
-                self.x_offset = self.x_offset + 1
+                self.x_offset = self.x_offset + self.speed
 
     def get(self):
         self.move()
         return super().get()
 
-    
 ###################################################
-class cloud(MoveAbleObject):
+class Background():
 
-    def __init__(self, Direction, Y): 
-        super().__init__(Direction, Y)
-        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_CYAN)
-        cloudString = """
-            ,((_'-. _
-        _(   (_ ) ),--.
-        (  (  __   _)  )-._
-        """
-        self.points = parseTxtToDrawableObject(cloudString, curses.color_pair(2)) 
+    def __init__(self):
+        self.cnt = 0
+        self.UpdateMultiplikator = 4
+        self.Background()
+        self.layer = 0
+
+    def Background(self):
+        background = []
+        x = 0
+        y = 0
+        while(True):
+            segment = Water().get()
+            segment.placeAt(x,y)
+            background.extend(segment.get())
+            x += segment.width
+            if x > curses.COLS and y <= curses.LINES:
+                x = 0
+                y += 1
+            if x > curses.COLS and y > curses.LINES:
+                self.oldBackground = background
+                return background
 
 
-
+    def get(self):
+        if self.cnt < self.UpdateMultiplikator:
+            self.cnt += 1
+            return self.oldBackground
+        else:
+            self.cnt = 0
+            retval = self.Background()
+            return retval  
 ###################################################
-class Wave(MoveAbleObject):
 
-    def __init__(self, Direction, Y):
-        super().__init__(Direction, Y)
-        waveString = u'hello わたし'
-        self.points = parseTxtToDrawableObject(waveString, curses.color_pair(1)) 
+class Easterhenn(DrawableObject):
+    def __init__(self):
+        super().__init__(EastereggLayer, chickenString, -100, -100, COLOR_WATER , False)
+        self.cnt = 0
+        self.old = []
+
+    def get(self):
+        retval = []
+        if self.cnt < 5:
+            self.cnt += 1
+        if self.cnt == 5:
+            self.cnt += 1
+            newX = random.randrange(0, curses.COLS, 1)
+            newY = random.randrange(self.height, curses.LINES, 1)
+            self.placeAt(newX, newY)
+            retval = super().get()
+        if (self.cnt > 5) & (self.cnt < 10):
+            self.cnt += 1
+            retval = super().get()
+        if self.cnt == 10:
+            self.cnt = 0
+        return retval
+
+
 
 
 
@@ -117,85 +242,161 @@ class Wave(MoveAbleObject):
 
 ###################################################
 class Water:
-
     def __init__(self):
-        pass
+        C = COLOR_WATER
+        waterSegments = []
+        X = 0
+        Y = 0
+        waterSegments.append(DrawableObject(BackGroundLayer, ' _=_=-=', X, Y, C, True))
+        waterSegments.append(DrawableObject(BackGroundLayer, ' ==-', X, Y, C, True))
+        waterSegments.append(DrawableObject(BackGroundLayer, ' =-', X, Y, C, True))
+        waterSegments.append(DrawableObject(BackGroundLayer, ' -=.--', X, Y, C, True))
+        waterSegments.append(DrawableObject(BackGroundLayer, ' _=-=-', X, Y, C, True))
+        waterSegments.append(DrawableObject(BackGroundLayer, ' -_=-=_', X, Y, C, True))
+        waterSegments.append(DrawableObject(BackGroundLayer, ' =-=-_-__=_-=', X, Y, C, True))
+        waterSegments.append(DrawableObject(BackGroundLayer, ' _=_=-=_', X, Y, C, True))
+        waterSegments.append(DrawableObject(BackGroundLayer, ' _             _', X, Y, C, True))
+        # return a random water segment from the set
+        self.segment = waterSegments[random.randrange(0, len(waterSegments), 1)]
 
     def get(self):
-        return self.Background()
-
-    def Background(self):
-        c = ' '
-        background = []
-        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_CYAN)
-        for y in range(curses.LINES -2):
-            for x in range(curses.COLS -2):
-                background.append(GraphicsPoint(x, y, c, curses.color_pair(1)))
-        return background
-
+        return self.segment
    
-
 ###################################################
 
 class Screen:
+    layers = []
+    for _ in range(100):
+        layers.append([])
+       
+    drawables = []
 
-    def __init__(self):
-        self.drawables = []
-        self.size = os.get_terminal_size()
-        self.cursesScreen = curses.initscr()
-        self.cursesScreen = curses.newwin(curses.LINES -1 ,curses.COLS -1)
-        self.cursesScreen.keypad(0)
-    
-    def addDrawable(self, obj):
-        self.drawables.append(obj)
+    size = os.get_terminal_size()
+    cursesScreen = curses.initscr()
+    cursesScreen = curses.newwin(curses.LINES -1 ,curses.COLS -1)
+    cursesScreen.keypad(0)
+   
+    def addDrawable(obj):
+        if isinstance(obj, list):
+            for o in obj:
+               Screen.layers[obj.layer].append(o)
+        else:
+            Screen.layers[obj.layer].append(obj)
 
-    def draw(self):
-        self.cursesScreen.clear()
-        for drawable in self.drawables:
-            for p in drawable.get():
-                if p.X < curses.COLS -2 and p.X >= 0:
-                    self.cursesScreen.addch(p.Y, p.X, p.C, p.Color)
-        self.cursesScreen.refresh()
+    def removeDrawable(obj):
+        Screen.layers[obj.layer].remove(obj)
+
+    def draw():
+        Screen.cursesScreen.erase()
+
+        for i in range(len(Screen.layers)):
+            if Screen.layers[i] == []:
+                continue
+            for drawable in Screen.layers[i]:
+                for p in drawable.get():
+                    if p.X < curses.COLS -2 and p.X >= 0 and p.Y < curses.LINES - 1 and p.Y >= 0:
+                        Screen.cursesScreen.addstr(p.Y, p.X, p.C,curses.color_pair(p.Color))
+        Screen.cursesScreen.refresh()
+
+
+##################################################
+class Cloud(MoveAbleObject):
+
+    def __init__(self, X, Y, Direction):
+        self.id = random.randrange(1,100,1)
+        cloudsStrings = []
+        cloudsStrings.append(cloudString1)
+        cloudsStrings.append(cloudString2)
+        cloudsStrings.append(cloudString3)
+        cloudString = cloudsStrings[random.randrange(0, len(cloudsStrings), 1)]
+
+        super().__init__( CloudLayer, cloudString, X, Y, COLOR_CLOUD, Direction, 10, True)
+
+        # construct a shadow for the cloud
+        self.shadow = MoveAbleObject(CloudLayer, cloudString, X-3,Y + self.height + 3, COLOR_SHADOW , Direction, 10, True)
+
+    def __del__(self):
+        del self.shadow
+###################################################
+class Island(DrawableObject):
+
+    def __init__(self, Layer, X, Y):
+        super().__init__(Layer, islandString, X, Y, COLOR_BROWN, False)
 
 
 ###################################################
-
 class Game:
 
     def __init__(self):
-        locale.setlocale(locale.LC_ALL,"")
-        self.screen = Screen()
-        self.water = Water()
-        self.wave = Wave("left", 3)
-        self.cloud = cloud("left", 7)
-        self.screen.addDrawable(self.water)
-        self.screen.addDrawable(self.cloud)
-        self.screen.addDrawable(self.wave)
+        self.updateIntervall = 0.25
+        self.cnt = 0
 
+        self.e = Easterhenn()
+        #self.island = Island(IslandLayer, 3, curses.LINES - 12)
+
+        locale.setlocale(locale.LC_ALL,"")
+        self.background = Background()
+        Screen.addDrawable(self.background)
+
+        #Create self moving clouds and place them initially out of sight
+        self.clouds = []
+        cloud1 = Cloud(-100, -100, "left")
+        cloud2 = Cloud(-100, -100, "left")
+        cloud3 = Cloud(-100, -100, "left")
+        cloud4 = Cloud(-100, -100, "left")
+        self.clouds.append(cloud1)
+        self.clouds.append(cloud2)
+        self.clouds.append(cloud3)
+        self.clouds.append(cloud4)
 
     def gameLoop(self):
+        time.sleep(self.updateIntervall)
         self.logicLoop()
         self.graphicsLoop()
-    
+   
     def logicLoop(self):
-        if self.cloud.x_offset == -10:
-            del self.cloud
-            self.cloud = cloud("left", 5)
-            self.screen.addDrawable(self.cloud)
+        self.cloudLogic()
+
+    def cloudLogic(self):
+        # create new clouds after old ones have left the screen
+        tempNewClouds = []
+        for i in range(len(self.clouds)):
+            cloud = self.clouds[i]
+            if cloud.x_offset <= -10:
+                newX = curses.COLS + random.randrange(0, curses.COLS, 1)
+                newY = random.randrange(5, curses.LINES - 5, 1)
+                Screen.removeDrawable(cloud)
+                newCloud = Cloud(newX, newY, "left")
+                tempNewClouds.append(newCloud)
+        for cloud in self.clouds:
+            del cloud
+        self.clouds = tempNewClouds
 
     def graphicsLoop(self):
-        self.screen.draw()
+        Screen.draw()
 
+def Main(scr):
 
+    curses.initscr()  
+    curses.start_color()
+    curses.curs_set(False)
+    curses.noecho()
 
-if __name__ == "__main__":
-    thread = Thread(target = detect_key_press)
-    thread.start() # keypress detection
-
+    curses.init_pair(COLOR_WATER, 27, 33) # lightBlue/Blue for water
+    curses.init_pair(COLOR_CLOUD, 253, 33) # white on blue for clouds
+    curses.init_pair(COLOR_SHADOW, 242,33) #gray on blue for cloudshadow
+    curses.init_pair(COLOR_BROWN, 96, 33) #
 
     game = Game()
 
-    while not key_pressed:
-        game.gameLoop()
-        time.sleep(0.1)
-    curses.endwin()
+    while True:
+        try:
+            game.gameLoop()
+           
+        except:
+            endCurses()
+            sys.exit(0)
+
+
+if __name__ == "__main__":
+    cursesWrapper(Main)
